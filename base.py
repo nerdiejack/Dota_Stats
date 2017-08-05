e3391c92de0d4a5f3f76e474a75628477c52b494
Jack Nerdie <nerdiejack@gmail.com>
2017-08-05 23:10:32 +0200

    Playing around with different plots.

--- base.py
+++ base.py
@@ -3,6 +3,19 @@ from matplotlib import pyplot as plt
 
 import seaborn as sns
 
-df = pd.read_csv('data_files/hero_base.csv', index_col=0)
-print df
+df = pd.read_csv('data_files/hero_base.csv')
+atk_df = df.drop(['STR', 'STR+', 'STR25', 'AGI', 'AGI+', 'AGI25', 'INT', 'INT+', 'INT25', 'T', 'T+',
+                  'T25', 'MS', 'AR', 'DMG_MIN', 'DMG_MAX', 'RG', 'BAT', 'VS-D', 'VS-N', 'TR', 'COL', 'HPReg', 'L'], axis=1)
+print atk_df
+
+attribute_type_colors = ['#ff0000', '#0000ff', "#00ff00"]
+
+sns.set_style('whitegrid')
+# sns.lmplot(x='ATK_S', y='DMG_MAX', data=df, hue='Attribute', fit_reg=False)
+# sns.violinplot(x="Attribute", y="AGI+", data=df, palette=attribute_type_colors, inner=None)
+# sns.swarmplot(x="Attribute", y="AGI+", data=df, color='k', alpha=0.7)
+sns.boxplot(data=atk_df)
+
+plt.plot()
+plt.show()
 
